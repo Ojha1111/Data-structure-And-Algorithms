3f6cd81244fb43164149b6b9e838a8175080627b
Amit Kumar Ojha <32640038+Ojha1111@users.noreply.github.com>
2018-10-10 21:28:05 +0530

    Update aman.cpp

--- aman.cpp
+++ aman.cpp
@@ -3,29 +3,21 @@
 #include <unistd.h>
 int main()
 {
-    // Fork returns process id
-    // in parent process
+    
     pid_t child_pid = fork();
- 
-    // Parent process 
-    if (child_pid > 0)
-        sleep(40);
- 
-    // Child process
-    else       
+      if (child_pid > 0)
+        sleep(40)
+	      else       
         exit(0);
-	// Create a child process      
-    int pid1 = fork();
+	int pid1 = fork();
 	int pid2 = fork();
- 
-    if (pid1 > 0)
+        if (pid1 > 0)
         printf("in parent process");
     else if (pid1 == 0)
     {
         sleep(20);
         printf("in child process");
     }
-	 
 	 if (pid2 > 0)
         printf("in parent process");
     else if (pid2== 0)
